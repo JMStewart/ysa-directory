1871d97f49f06929f84b5144c198d3378b44e462
James Stewart <james.stewart@domo.com>
2016-09-11 00:27:03 -0600

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,48 @@
+require('./styles/app.css');
+var $ = require('jquery');
+var parse = require('dank-csv');
+var Vue = require('vue');
+
+
+$.get('/directory.csv')
+  .then(res => parse(res.replace(/\r/g, '\n')))
+  .then(createAllPhotoUrls)
+  .then(groupByApt)
+  .then(init);
+
+function createAllPhotoUrls(people) {
+  return people.map(createPhotoUrl);
+}
+
+function createPhotoUrl(person) {
+  person.photoUrl = '/photos/' + person.photo + '.jpg';
+  return person;
+}
+
+function groupByApt(people) {
+  var apts = {};
+  people.forEach(person => {
+    var addr = person.addr1 + '-' + person.addr2;
+    console.log(addr);
+    if (!apts[addr]) {
+      apts[addr] = {
+        addr1: person.addr1,
+        addr2: person.addr2,
+        people: []
+      };
+    }
+    apts[addr].people.push(person);
+  });
+  return apts;
+}
+
+function init(data) {
+  console.log(data);
+  new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue.js!',
+      apts: data
+    }
+  });
+}
